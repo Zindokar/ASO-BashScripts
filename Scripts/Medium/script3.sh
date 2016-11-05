abc6f56f824c7d27bd288e093c0f271ac7dd96c9
Alejandro <alejandro@alsnet.es>
2016-11-05 02:33:16 +0000

    improved script
    - sorting the output of the find in order that uniq -c works properly
    - adding extra pipe to the while sentence in order to remove the adding
    extra file trick

--- Scripts/Medium/script3.sh
+++ Scripts/Medium/script3.sh
@@ -7,16 +7,13 @@ then
 fi
 echo "Buscando ficheros en el sistema....."
 echo "--------"
-$( find /home -type f -exec stat -c '%U' {} \; | uniq -c | tr -s " " > auxFile.txt )
-exec 3<auxFile.txt
-while IFS=' ' read -u 3 numFicheros usuario
+find / -type f -printf "%u\n" | sort | uniq -c | tr -s " " | while IFS=' ' read numFicheros usuario
 do
 	if [ $numFicheros -ge $1 ]
 	then
 		echo "$numFicheros ficheros pertenecen al usuario $usuario."
 	fi
 done
-$( rm -f auxFile.txt )
 echo "--------"
 echo "Ejecución completa."
 exit 1
