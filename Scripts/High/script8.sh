1c35ec4fb1b2fc9f0c116e184b41cca6d8b5459d
Alejandro <alejandro@alsnet.es>
2016-11-08 10:52:49 +0000

    fixed grep precision
    added -x option

--- Scripts/High/script8.sh
+++ Scripts/High/script8.sh
@@ -2,7 +2,7 @@
 #Autor:Alejandro Lopez Santos
 cut -d ":" -f 7 /etc/passwd | while read bashOption
 do
-    isFound=$( grep $bashOption /etc/shells )
+    isFound=$( grep -x $bashOption /etc/shells )
     if [ -z $isFound ]
     then
         echo $bashOption
